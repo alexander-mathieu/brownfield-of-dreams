75dea642ef5119e6df6d90ecf6eacd60e488e93b
Brian Plantico <bplantico@gmail.com>
2019-07-02 16:35:54 -0600

    Adds spec/features/user/user_sees_github_repos_on_dashboard.rb
    Co-Authored-By: Alexander Mathieu <47018937+alexander-mathieu@users.noreply.github.com>
    Co-Authored-By: Brennan Ayers <45154998+BrennanAyers@users.noreply.github.com>

---
+++ spec/features/user/user_sees_github_repos_on_dashboard.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe 'As a logged in User' do
+  describe 'when I visit my dashboard' do
+    it 'displays github section with five repositories' do
+      user = create(:user)
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+      visit dashboard_path
+
+      expect(page).to have_content("GitHub Repositories")
+      expect(page).to have_link("Repo Number 1")
+      expect(page).to have_link("Repo Number 2")
+      expect(page).to have_link("Repo Number 3")
+      expect(page).to have_link("Repo Number 4")
+      expect(page).to have_link("Repo Number 5")
+      expect(page).to_not have_link("Repo Number 6")
+
+    end
+  end
+end
