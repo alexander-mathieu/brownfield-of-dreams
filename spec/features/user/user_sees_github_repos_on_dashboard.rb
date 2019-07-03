1696f059fe83a4e97d9eecea807f7d82e4388a40
Brian Plantico <bplantico@gmail.com>
2019-07-02 18:12:33 -0600

    Adds additional check for url in repository links.
    Co-Authored-By: Alexander Mathieu <47018937+alexander-mathieu@users.noreply.github.com>
    Co-Authored-By: Brennan Ayers <45154998+BrennanAyers@users.noreply.github.com>

--- spec/features/user/user_sees_github_repos_on_dashboard.rb
+++ spec/features/user/user_sees_github_repos_on_dashboard.rb
@@ -11,11 +11,11 @@ RSpec.describe 'As a logged in User' do
       end
 
       expect(page).to have_content("GitHub Repositories")
-      expect(page).to have_link("Repo Number 1")
-      expect(page).to have_link("Repo Number 2")
-      expect(page).to have_link("Repo Number 3")
-      expect(page).to have_link("Repo Number 4")
-      expect(page).to have_link("Repo Number 5")
+      expect(page).to have_link("Repo Number 1", href:"https://github.com/alexander-mathieu/brownfield-of-dreams")
+      expect(page).to have_link("Repo Number 2", href: "https://github.com/bplantico/1903_final")
+      expect(page).to have_link("Repo Number 3", href: "https://github.com/bplantico/activerecord-obstacle-course")
+      expect(page).to have_link("Repo Number 4", href: "https://github.com/bplantico/active_record_obstacle_course")
+      expect(page).to have_link("Repo Number 5", href: "https://github.com/bplantico/apollo_14")
       expect(page).to_not have_link("Repo Number 6")
 
     end
