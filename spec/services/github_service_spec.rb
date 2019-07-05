9f42feeca68f6f88a3eac909cdc963b8fff1bbb1
Alexander Mathieu <alexander.mathieu@icloud.com>
2019-07-05 12:16:55 -0600

    Add GitHub service followers_by_user spec with fixture
    Co-Authored-By: Brian Plantico <43261385+bplantico@users.noreply.github.com>
    Co-Authored-By: Brennan Ayers <45154998+BrennanAyers@users.noreply.github.com>

--- spec/services/github_service_spec.rb
+++ spec/services/github_service_spec.rb
@@ -7,8 +7,8 @@ RSpec.describe GithubService do
     expect(subject).to be_a GithubService
   end
 
-  context '#repos_by_user' do
-    it 'returns valid repos when user token is passed in' do
+  context '#repositories_by_user' do
+    it 'returns repos when valid user token is passed in' do
       stub_data = File.read('./fixtures/github_repos_by_user.json')
 
       stub_request(:get, 'https://api.github.com/user/repos').to_return(status: 200, body: stub_data)
@@ -19,6 +19,22 @@ RSpec.describe GithubService do
       expect(user_repos.count).to eq(7)
       expect(user_repos.first).to be_a Hash
       expect(user_repos.first).to have_key(:name)
+      expect(user_repos.first).to have_key(:html_url)
+    end
+  end
+
+  context '#followers_by_user' do
+    it 'returns followers when a valid user token is passed in' do
+      stub_data = File.read('./fixtures/github_followers_by_user.json')
+
+      stub_request(:get, 'https://api.github.com/user/followers').to_return(status: 200, body: stub_data)
+
+      user_repos = subject.followers_by_user
+
+      expect(user_repos).to be_an Array
+      expect(user_repos.count).to eq(5)
+      expect(user_repos.first).to be_a Hash
+      expect(user_repos.first).to have_key(:login)
       expect(user_repos.first).to have_key(:html_url)
     end
   end
