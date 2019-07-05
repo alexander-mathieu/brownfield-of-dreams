4362f7d148df27b74fb77146c47b6039345bf4a9
Alexander Mathieu <alexander.mathieu@icloud.com>
2019-07-05 12:16:01 -0600

    Add GitHub::User with initialize
    Co-Authored-By: Brian Plantico <43261385+bplantico@users.noreply.github.com>
    Co-Authored-By: Brennan Ayers <45154998+BrennanAyers@users.noreply.github.com>

---
+++ app/models/git_hub/user.rb
@@ -0,0 +1,11 @@
+module GitHub
+  class User
+    attr_reader :name,
+                :html_url
+
+    def initialize(attributes)
+      @name = attributes[:login]
+      @html_url  =  attributes[:html_url]
+    end
+  end
+end
