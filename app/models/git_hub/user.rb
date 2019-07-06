daa83ac0d06377a7ff457e46427ad37b4e7c6595
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-07-06 17:09:13 -0600

    Add #registered? method to Github::User model to check our User DB for Github UID
    Co-Authored-By: Brennan Ayers <45154998+BrennanAyers@users.noreply.github.com>
    Co-Authored-By: Brian Plantico <43261385+bplantico@users.noreply.github.com>
    Co-Authored-By: Alexander Mathieu <47018937+alexander-mathieu@users.noreply.github.com>

--- app/models/git_hub/user.rb
+++ app/models/git_hub/user.rb
@@ -1,11 +1,15 @@
-module GitHub
-  class User
-    attr_reader :name,
-                :html_url
+class GitHub::User
+  attr_reader :name,
+              :html_url,
+              :uid
 
-    def initialize(attributes)
-      @name = attributes[:login]
-      @html_url = attributes[:html_url]
-    end
+  def initialize(attributes)
+    @name = attributes[:login]
+    @html_url = attributes[:html_url]
+    @uid = attributes[:id]
+  end
+
+  def registered?
+    !User.find_by(github_uid: @uid).nil?
   end
 end
