889758c97c952b79f8490cbfc26b1b380c023c73
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-07-06 15:21:30 -0600

    Add initial spec for User Friendship view
    Co-Authored-By: Brennan Ayers <45154998+BrennanAyers@users.noreply.github.com>
    Co-Authored-By: Brian Plantico <43261385+bplantico@users.noreply.github.com>
    Co-Authored-By: Alexander Mathieu <47018937+alexander-mathieu@users.noreply.github.com>

---
+++ spec/features/user/user_can_make_friends_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe 'As a registered user', type: :feature do
+  context 'When I visit my dashboard' do
+    before :each do
+      @user = create(:user, github_token: '12345', github_uid: '09876')
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+    end
+
+    it 'I can see a Friendship section' do
+      visit dashboard_path
+
+      within('.friendships') do
+        expect(page).to have_content('Friends')
+        expect(page).to have_content('You have no friends :(')
+      end
+    end
+  end
+end
