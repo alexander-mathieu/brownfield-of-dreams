2b8a8764c3bbc68ecee2da42e1154324ecfa13d5
Alexander Mathieu <alexander.mathieu@icloud.com>
2019-07-07 14:00:21 -0600

    Add :classroom_tutorial to tutorial factory

--- spec/factories/tutorials.rb
+++ spec/factories/tutorials.rb
@@ -4,6 +4,13 @@ FactoryBot.define do
     description { Faker::HitchhikersGuideToTheGalaxy.marvin_quote }
     thumbnail { 'http://cdn3-www.dogtime.com/assets/uploads/2011/03/puppy-development-460x306.jpg' }
     playlist_id { Faker::Crypto.md5 }
-    classroom { false }
+  end
+
+  factory :classroom_tutorial, parent: :tutorial do
+    title { Faker::Name.unique.name }
+    description { Faker::HitchhikersGuideToTheGalaxy.marvin_quote }
+    thumbnail { 'http://cdn3-www.dogtime.com/assets/uploads/2011/03/puppy-development-460x306.jpg' }
+    playlist_id { Faker::Crypto.md5 }
+    classroom { true }
   end
 end
