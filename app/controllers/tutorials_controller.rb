731b461c8ab629522bffde9cb01511671de25439
Alexander Mathieu <alexander.mathieu@icloud.com>
2019-07-07 18:02:21 -0600

    Add logic to TutorialsController to prevent unregistered users from viewing classroom tutorial show pages

--- app/controllers/tutorials_controller.rb
+++ app/controllers/tutorials_controller.rb
@@ -1,6 +1,11 @@
 class TutorialsController < ApplicationController
   def show
     tutorial = Tutorial.find(params[:id])
-    @facade = TutorialFacade.new(tutorial, params[:video_id])
+    if tutorial.classroom?
+      require_current_user
+      @facade = TutorialFacade.new(tutorial, params[:video_id])
+    else
+      @facade = TutorialFacade.new(tutorial, params[:video_id])
+    end
   end
 end
