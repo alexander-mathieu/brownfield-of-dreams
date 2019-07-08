5da2cc9e738e38d934b3d3d3c39933f90e8796dd
Alexander Mathieu <alexander.mathieu@icloud.com>
2019-07-07 18:30:03 -0600

    Remove unnecessary space in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -125,6 +125,6 @@ m3_tutorial.videos.create!({
   'position' => 6
 })
 
-User.create!(email: 'admin@example.com', first_name: 'Bossy', last_name: 'McBosserton', password:  'password', role: :admin)
+User.create!(email: 'admin@example.com', first_name: 'Bossy', last_name: 'McBosserton', password: 'password', role: :admin)
 User.create!(email: 'user1@example.com', first_name: 'Ricky', last_name: 'Bobby', password: 'password', github_token: ENV['GITHUB-TOKEN'], role: :default)
 User.create!(email: 'user2@example.com', first_name: 'Jean', last_name: 'Girard', password: 'password')
