b57e389e9777d38890df499703511a0765558fda
Alexander Mathieu <alexander.mathieu@icloud.com>
2019-07-08 19:35:15 -0600

    Add spec for accepting nested attributes for Videos to Tutorial model

--- spec/models/tutorial_spec.rb
+++ spec/models/tutorial_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe Tutorial, type: :model do
   describe 'relationships' do
     it { should have_many :videos }
+    it { should accept_nested_attributes_for :videos }
   end
 
   describe 'instance methods' do
