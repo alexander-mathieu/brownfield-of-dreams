89008eacce267297bbe6a10ad0354dfdd191e2ce
Alexander Mathieu <alexander.mathieu@icloud.com>
2019-07-09 15:31:45 -0600

    Add create action to InviteController
    Co-Authored-By: Brian Plantico <43261385+bplantico@users.noreply.github.com>
    Co-Authored-By: Brennan Ayers <45154998+BrennanAyers@users.noreply.github.com>

--- app/controllers/invite_controller.rb
+++ app/controllers/invite_controller.rb
@@ -1,5 +1,21 @@
 class InviteController < ApplicationController
   def new
+  end
 
+  def create
+    email = github_service.email_query(params[:github_handle])
+    if email
+      InviteMailer.invite_email(current_user, email).deliver_now
+      flash[:success] = 'Successfully sent invite!'
+    else
+      flash[:danger] = "The Github user you selected doesn't have an email address associated with their account."
+    end
+    redirect_to dashboard_path
+  end
+
+  private
+
+  def github_service
+    GithubService.new(ENV['GITHUB-TOKEN'])
   end
 end
