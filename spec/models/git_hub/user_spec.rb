1584570c1cc06cffc50ca46f87e1890969a624b3
Alexander Mathieu <alexander.mathieu@icloud.com>
2019-07-08 20:06:25 -0600

    Add spec/models/git_hub with specs for GitHub::User and GitHub::Repository

---
+++ spec/models/git_hub/user_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe GitHub::User do
+  before :each do
+    attributes = { login: "Joey Bagodonuts",
+                   html_url: "https://joeytime.com" }
+
+    @user = GitHub::User.new(attributes)
+  end
+
+  it 'existance' do
+    expect(@user).to be_a(GitHub::User)
+  end
+
+  it 'attributes' do
+    expect(@user.name).to eq("Joey Bagodonuts")
+    expect(@user.html_url).to eq("https://joeytime.com")
+  end
+end
