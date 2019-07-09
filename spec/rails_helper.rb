9c5fa444ef7b837389f7fc78ed16942ec7398bcf
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-07-08 23:15:25 -0600

    Add #appear_before matchers to RSpec configuration

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -50,3 +50,9 @@ RSpec.configure do |config|
 
   config.filter_rails_from_backtrace!
 end
+
+RSpec::Matchers.define :appear_before do |later_content|
+  match do |earlier_content|
+    page.body.index(earlier_content) < page.body.index(later_content)
+  end
+end
