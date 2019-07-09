dcb01abb97eed9de6a5654701d976d43a3b6b12d
Alexander Mathieu <alexander.mathieu@icloud.com>
2019-07-09 15:30:40 -0600

    Add route for post to invite#create
    Co-Authored-By: Brian Plantico <43261385+bplantico@users.noreply.github.com>
    Co-Authored-By: Brennan Ayers <45154998+BrennanAyers@users.noreply.github.com>

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,11 +29,15 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
+  # SMTP configuration with ActionMailer
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { address: 'localhost', port: 1025 }
+
   # Store uploaded files on the local file system (see config/storage.yml for options)
   config.active_storage.service = :local
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  # Do care if the mailer can't send.
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
 
