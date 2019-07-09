03734cec009c9022e0c51f5895e918cdc154eb09
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-07-09 13:35:20 -0600

    Hound CI fixes pt2
    Co-Authored-By: Brennan Ayers <45154998+BrennanAyers@users.noreply.github.com>
    Co-Authored-By: Brian Plantico <43261385+bplantico@users.noreply.github.com>
    Co-Authored-By: Alexander Mathieu <47018937+alexander-mathieu@users.noreply.github.com>

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 class FriendshipsController < ApplicationController
   def create
     new_friend = User.find_by(github_uid: friendship_params[:uid])
-    friendship = Friendship.new({user: current_user, friend: new_friend})
+    friendship = Friendship.new(user: current_user, friend: new_friend)
     if friendship.save
       flash[:notice] = "Congrats, you are now friends with #{new_friend.first_name + ' ' + new_friend.last_name}!"
     else
--- app/models/git_hub/user.rb
+++ app/models/git_hub/user.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 class GitHub::User
   attr_reader :name,
               :html_url,
