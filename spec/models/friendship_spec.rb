4361944eac7700183b7e70920caffd0f94aef6d5
Alexander Mathieu <alexander.mathieu@icloud.com>
2019-07-10 17:26:10 -0600

    Add validations and real_friends private method to Friendship model with specs
    Co-Authored-By: Brian Plantico <43261385+bplantico@users.noreply.github.com>
    Co-Authored-By: Brennan Ayers <45154998+BrennanAyers@users.noreply.github.com>

--- spec/models/friendship_spec.rb
+++ spec/models/friendship_spec.rb
@@ -3,8 +3,23 @@
 require 'rails_helper'
 
 RSpec.describe Friendship, type: :model do
+  describe 'validations' do
+    it { should validate_presence_of :user_id }
+    it { should validate_presence_of :friend_id }
+  end
+
   describe 'relationships' do
     it { should belong_to :user }
     it { should belong_to :friend }
+  end
+
+  describe 'instance method' do
+    it '#real_friends' do
+      user = create(:user)
+      friendship = Friendship.new(user: user, friend: user)
+
+      expect(friendship.save).to be(false)
+      expect(friendship.errors.messages[:user].first).to eq("I guess I get what I deserve, don't I?")
+    end
   end
 end
