94f64029fb17beea9873cb4dd0723e8d6989027f
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-07-11 14:58:42 -0600

    Add flash message to UsersController#create for Logged in as new User for Dashboard
    Co-Authored-By: Brennan Ayers <45154998+BrennanAyers@users.noreply.github.com>
    Co-Authored-By: Brian Plantico <43261385+bplantico@users.noreply.github.com>
    Co-Authored-By: Alexander Mathieu <47018937+alexander-mathieu@users.noreply.github.com>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,7 @@ class UsersController < ApplicationController
     if user.save
       session[:user_id] = user.id
       redirect_to dashboard_path
+      flash[:success] = "Logged in as #{user.first_name + ' ' + user.last_name}"
     else
       flash[:error] = 'Username already exists'
       render :new
