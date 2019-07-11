e390723710ec45d0466fab1551d8992092c659c8
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-07-11 15:09:09 -0600

    Update User factory to have verified_email: true
    Co-Authored-By: Brennan Ayers <45154998+BrennanAyers@users.noreply.github.com>
    Co-Authored-By: Brian Plantico <43261385+bplantico@users.noreply.github.com>
    Co-Authored-By: Alexander Mathieu <47018937+alexander-mathieu@users.noreply.github.com>

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,6 +5,7 @@ FactoryBot.define do
     last_name { Faker::Artist.name }
     password { Faker::Color.color_name }
     role { :default }
+    verified_email { true }
   end
 
   factory :admin, parent: :user do
