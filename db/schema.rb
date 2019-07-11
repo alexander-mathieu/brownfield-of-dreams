4a958060249abecf9aa19a63b2804cea2c64df09
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-07-11 16:52:23 -0600

    Add verification_token attribute to User table
    Co-Authored-By: Brennan Ayers <45154998+BrennanAyers@users.noreply.github.com>
    Co-Authored-By: Brian Plantico <43261385+bplantico@users.noreply.github.com>
    Co-Authored-By: Alexander Mathieu <47018937+alexander-mathieu@users.noreply.github.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_11_204233) do
+ActiveRecord::Schema.define(version: 2019_07_11_225000) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -80,6 +80,7 @@ ActiveRecord::Schema.define(version: 2019_07_11_204233) do
     t.string "github_uid"
     t.string "github_login"
     t.boolean "verified_email", default: false
+    t.string "verification_token"
     t.index ["email"], name: "index_users_on_email"
   end
 
