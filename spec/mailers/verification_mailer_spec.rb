5ba64597bcba376759aff4fdf7db834962527ea1
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-07-11 16:37:41 -0600

    Add initial VerificationMailer spec
    Co-Authored-By: Brennan Ayers <45154998+BrennanAyers@users.noreply.github.com>
    Co-Authored-By: Brian Plantico <43261385+bplantico@users.noreply.github.com>
    Co-Authored-By: Alexander Mathieu <47018937+alexander-mathieu@users.noreply.github.com>

---
+++ spec/mailers/verification_mailer_spec.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe VerificationMailer, type: :mailer do
+  describe 'verification_email' do
+    before :each do
+      @user = create(:user, verified_email: false)
+      @mail = VerificationMailer.verification_email(@user)
+    end
+
+    it 'renders the headers' do
+      expect(@mail.subject).to eq('Brownest Fields Account Verification')
+      expect(@mail.to).to eq([@user.email])
+      expect(@mail.from).to eq(['no-reply@brownest-field.herokuapp.com'])
+    end
+
+    it 'renders the body' do
+      expect(@mail.body.encoded).to match('Thanks for registering! To confirm your registration, click the link below!')
+    end
+
+    it 'assigns @first_name' do
+      expect(@mail.body.encoded).to match(@user.first_name)
+    end
+
+    it 'assigns @confirmation_token' do
+      expect(@mail.body.encoded).to match(@user.confirmation_token)
+    end
+  end
+
+end
