aad993a800c441c2338df030c508aee9dcf46a74
Alexander Mathieu <alexander.mathieu@icloud.com>
2019-07-13 13:51:02 -0600

    Add memoization to UserShowFacade to cut down GitHub API calls
    Co-Authored-By: Brian Plantico <43261385+bplantico@users.noreply.github.com>

--- app/facades/user_show_facade.rb
+++ app/facades/user_show_facade.rb
@@ -7,25 +7,25 @@ class UserShowFacade
   end
 
   def followers
-    users = github_service.followers_by_user
+    @_github_followers ||= github_service.followers_by_user
 
-    users.map do |user|
-      GitHub::User.new(user)
+    @_github_followers.map do |follower|
+      GitHub::User.new(follower)
     end
   end
 
   def following
-    users = github_service.following_by_user
+    @_github_following ||= github_service.following_by_user
 
-    users.map do |user|
-      GitHub::User.new(user)
+    @_github_following.map do |following|
+      GitHub::User.new(following)
     end
   end
 
-  def repositories(quantity = 5)
-    repos = github_service.repositories_by_user
+  def repositories
+    @_repos ||= github_service.repositories_by_user
 
-    repos[0..(quantity - 1)].map do |repo|
+    @_repos.map do |repo|
       GitHub::Repository.new(repo)
     end
   end
@@ -37,6 +37,6 @@ class UserShowFacade
   private
 
   def github_service
-    GithubService.new(@token)
+    @_github_service ||= GithubService.new(@token)
   end
 end
