7cf8de19ac0c2d555974714f0ad09b90677c2797
Brennan Ayers <brennan.f.ayers@gmail.com>
2019-07-13 14:21:36 -0600

    Update Tutorial Factory to use correct YTimg urls

--- spec/factories/tutorials.rb
+++ spec/factories/tutorials.rb
@@ -2,14 +2,14 @@ FactoryBot.define do
   factory :tutorial do
     title { Faker::Name.unique.name }
     description { Faker::HitchhikersGuideToTheGalaxy.marvin_quote }
-    thumbnail { 'http://cdn3-www.dogtime.com/assets/uploads/2011/03/puppy-development-460x306.jpg' }
+    thumbnail { 'https://i.ytimg.com/vi/CStwQXbTa7c/hqdefault.jpg' }
     playlist_id { Faker::Crypto.md5 }
   end
 
   factory :classroom_tutorial, parent: :tutorial do
     title { Faker::Name.unique.name }
     description { Faker::HitchhikersGuideToTheGalaxy.marvin_quote }
-    thumbnail { 'http://cdn3-www.dogtime.com/assets/uploads/2011/03/puppy-development-460x306.jpg' }
+    thumbnail { 'https://i.ytimg.com/vi/CStwQXbTa7c/hqdefault.jpg' }
     playlist_id { Faker::Crypto.md5 }
     classroom { true }
   end
